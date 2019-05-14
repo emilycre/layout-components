9cbdceff8c625b1868305525f37551e70b1eeba6
Emily Creamer <emily.creamer@icloud.com>
2019-05-14 15:37:51 -0700

    webpack play

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hello.');
+import styles from './index.css';
+const root = document.getElementById('root');
+
+const dogs = ['Neil', 'Frank', 'Fido', 'Spot'];
+
+const ul = document.createElement('ul');
+ul.className = styles.dogs;
+root.appendChild(ul);
+
+dogs.forEach(dog => {
+  const li = document.createElement('li');
+  li.textContent = dog;
+  ul.appendChild(li);
+});
